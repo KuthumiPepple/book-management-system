eb5eb23da7d1e8f6cbc567ea769409323e49052b
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-06-07 12:43:51 +0100

    Create routes

--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/kuthumipepple/book-management-system/pkg/controllers"
+)
+
+var RegisterBookRoutes = func(router *mux.Router){
+	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
+	router.HandleFunc("/books/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("books/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("books/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
