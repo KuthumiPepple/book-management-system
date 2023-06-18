3d1362339e0e9c1c1944a6503522cb8706974840
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-06-18 08:28:00 +0100

    add main function

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/kuthumipepple/book-management-system/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:5000", r))
+}
