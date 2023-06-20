ffe97fec763eacd117213c161c2382b9d4fbc6ab
Kuthumi Pepple <kuthumipepple@gmail.com>
2023-06-20 12:34:45 +0100

    fix: add missing forward slash to endpoint URLs

--- pkg/routes/book-routes.go
+++ pkg/routes/book-routes.go
@@ -9,6 +9,6 @@ var RegisterBookRoutes = func(router *mux.Router){
 	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
 	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
 	router.HandleFunc("/books/{bookId}", controllers.GetBookById).Methods("GET")
-	router.HandleFunc("books/{bookId}", controllers.UpdateBook).Methods("PUT")
-	router.HandleFunc("books/{bookId}", controllers.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/books/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/books/{bookId}", controllers.DeleteBook).Methods("DELETE")
 }
